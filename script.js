425cacf1784c9dde6a1f51580b5986ca8afe7e3c
Peter Gryllis <petergryllis@riacci.com>
2022-01-23 18:12:22 +1100

    js updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const API_KEY = "d44bc17c2091a2e0a516eae08c6d721d";
 let CitySelectButton = document.getElementById("search-city-submit-button");
 CitySelectButton.addEventListener("click", function() {
     let InputofCity = document.getElementById("search-city-form");
-    Name_of_City = InputofCity.value;
+    city_selection = InputofCity.value;
     getCity(InputofCity.value);
     InputofCity.value = "";
 });
@@ -16,3 +16,135 @@ function getCity(city) {
 }
 
 
+//Set up function to obtain the weather data from API
+
+function getDATA(URL_Link) {
+    fetch(URL_Link)
+    .then(function(response) {
+       if (response.ok) {
+        response.json()
+        .then(function(data) {
+            function getCityData(data) {
+                console.log(data);
+                let currentWeatherTitle = document.getElementById("current-weather-title");
+                let name = data.name;
+                currentWeatherTitle.textContent = name
+                
+                let cityUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+
+                let latitude = data.coord.lat;
+                let longitude = data.coord.lon;
+
+
+                fetch(cityUrl)
+                .then(function(response) {
+                    response.json()
+                    .then(function(data) {
+                        let dateTime = new Date(data.current.dt * 1000).toLocaleString();
+                        dateTime = dateTime.split(",");
+                        dateTime = dateTime[0];
+                        let currentIcon = document.getElementById("todays-image");
+                        let icon = data.current.weather[0].icon;
+                        currentIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+                        currentWeatherTitle.textContent = name + " (" + dateTime + ") ";
+                        currentIcon.style.display = "block";
+                        getForecast(data);
+                        displayData(data);
+                    });
+                });
+            }
+            getCityData(data);
+        });
+        if (city_name != "") {
+            createSavedList(city_name);
+        }
+       }
+    });
+}
+
+
+
+
+// Function to handle displaying the data
+function displayData(weatherData) {
+    // Obtain weather data from API to display information--
+
+    let temperature = document.getElementById("temp");
+    let wind = document.getElementById("Wind");
+    let humidity = document.getElementById("Humidity");
+    let uvIndex = document.getElementById("UVIndex")
+
+    
+    // Extract userful information from the data
+    let currentTemp = weatherData.current.feels_like;
+    let windSpeed = weatherData.current.wind_speed;
+    let currentHumidity = weatherData.current.humidity;
+    let uvi = weatherData.current.uvi;
+
+    // Assign the data obtained from the API to the HTML values which are obtained from the to the api
+    temperature.textContent = "Temperature: " + currentTemp + " C";
+    wind.textContent = "Wind: " + windSpeed + " km/hr";
+    humidity.textContent = "Humidity: " + currentHumidity + " %";
+    uvIndex.textContent = uvi;
+
+    // Filter color codes based on UV value
+    if (uvi <= 4 ) {
+        uvIndex.style.background = "green";
+    } else if (uvi > 4 && uvi <= 8) {
+        uvIndex.style.background = "yellow";
+    } else {
+        uvIndex.style.background = "red";
+    }
+}
+
+
+
+
+//forecast function 
+function getForecast(data) {
+    // get the card from the DOM'
+    let forecastContainer = document.getElementById("forecast-container");
+    forecastContainer.innerHTML = "";
+    // Get the next 5 days
+    for (let i=1; i<5; i++) {
+        let forecastCard = document.createElement("div");
+        forecastCard.classList.add("forecast-square");
+        forecastContainer.appendChild(forecastCard);
+        // get the date for each day
+        let dateTime = new Date(data.daily[i].dt * 1000).toLocaleString();
+        dateTime = dateTime.split(",");
+        dateTime = dateTime[0];
+        let forecastDate = document.createElement("h1");
+        forecastDate.classList.add("forecast-date");
+        forecastDate.textContent = dateTime;
+        forecastCard.appendChild(forecastDate);
+        // Get the icon for the weather
+        let icon = data.daily[i].weather[0].icon;
+        let forecastIcon = document.createElement("img");
+        forecastIcon.classList.add("forecast-icon");
+        forecastIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+        forecastCard.appendChild(forecastIcon);
+        // get the temperature for each day
+        let temperature = data.daily[i].feels_like.day;
+        let forecastTemperature = document.createElement("p");
+        forecastTemperature.textContent = "Temperature: " + temperature + " C";
+        forecastCard.appendChild(forecastTemperature);
+        forecastTemperature.classList.add("forecast-temp");
+        // get the wind for each day
+        let wind = data.daily[i].wind_speed;
+        let forecastWind = document.createElement("p");
+        forecastWind.textContent = "Wind: " + wind + " KM/H";
+        forecastCard.appendChild(forecastWind);
+        forecastWind.classList.add("forecast-wind");
+        // get the humidity for each day
+        let humidity = data.daily[i].humidity;
+        let forecastHumidity = document.createElement("p");
+        forecastHumidity.textContent = "Humidity: " + humidity + "%";
+        forecastCard.appendChild(forecastHumidity);
+        forecastHumidity.classList.add("forecast-humidity");
+    }
+    // get the date for each day
+    // get the icons
+    console.log(data);
+    // add each day to a card to be displayed at the bottom of the page
+}
